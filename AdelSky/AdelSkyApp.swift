09a75d54271af334790a08e8792db9e76b4b5156
AdelMohamedS <sadekm.adel@gmail.com>
2023-09-14 22:22:42 +0300

    Initial Commit

---
+++ AdelSky/AdelSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdelSkyApp.swift
+//  AdelSky
+//
+//  Created by Adel The Adroit  on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdelSkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
